f068842aba3048f78cff0c694a2ae0fedda4878f
Nolan Roose <roose.nolan@gmail.com>
2022-10-20 12:52:26 +0200

    feat(mask): better mask for input (#30)

--- projects/nolanroose/ngx-simple-datepicker/src/ngx-simple-datepicker.module.ts
+++ projects/nolanroose/ngx-simple-datepicker/src/ngx-simple-datepicker.module.ts
@@ -2,21 +2,15 @@ import {NgModule} from '@angular/core';
 import {NgxSimpleDatepickerComponent} from './components/ngx-simple-datepicker.component';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {IConfig, MaskPipe, NgxMaskModule} from 'ngx-mask';
 import {OnlyNumberDirective} from './directives/only-number.directive';
 import {DateHelper} from './helpers/date.helper';
 import {FormHelper} from './helpers/form.helper';
-
-const maskConfig: Partial<IConfig> = {
-  validation: false,
-  placeHolderCharacter: '_',
-  dropSpecialCharacters: true
-};
+import {TextMaskModule} from 'angular2-text-mask';
 
 @NgModule({
   declarations: [NgxSimpleDatepickerComponent, OnlyNumberDirective],
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, NgxMaskModule.forRoot(maskConfig)],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, TextMaskModule],
   exports: [NgxSimpleDatepickerComponent],
-  providers: [DateHelper, FormHelper, MaskPipe]
+  providers: [DateHelper, FormHelper]
 })
 export class NgxSimpleDatepickerModule {}
